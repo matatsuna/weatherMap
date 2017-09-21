7c43f9d8c12a861f3d84f1180f02725ab8b5b246
matatsuna <ev50510@meiji.ac.jp>
2017-09-21 14:55:15 +0900

    add program

---
+++ api/index.php
@@ -0,0 +1,31 @@
+<?php
+header("Content-Type: application/json; charset=utf-8");
+
+if($_REQUEST["month"]){
+    $month = htmlspecialchars($_REQUEST["month"]);
+}else{
+    $month = 1;
+}
+
+$mysqli=new mysqli ("localhost","UserName","Pass");
+$mysqli -> select_db("weather_db");
+$results=$mysqli->query("select area_table.prefecture_name,sum(rainfall_level) from weather_table,area_table where weather_table.prefecture_id=area_table.prefecture_id and month=".$month." and year=2008 group by weather_table.prefecture_id");
+$json = array();
+$jsonchild = array();
+
+array_push($jsonchild,"都道府県");
+array_push($jsonchild,"月別総雨量");
+array_push($json,$jsonchild);
+
+while ($line = $results-> fetch_array(MYSQLI_BOTH)) {
+
+    $jsonchild = array();
+
+    array_push($jsonchild,$line["prefecture_name"]);
+    array_push($jsonchild,intval($line["sum(rainfall_level)"]));
+    array_push($json,$jsonchild);
+}
+$mysqli->close();
+
+echo json_encode($json);
+?>
